54920173a628d5222c66b32ff3d02624388c6a69
RaulMun <raulmuniz38@gmail.com>
2024-06-18 14:39:22 -0600

    chipichipichapachapa :fire:

---
+++ pages/ProfilePage.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
+
+export function ProfilePage() {
+    return (
+        <View style={styles.maincontainer}>
+          
+        </View>
+      );
+    }
+
+    const styles = StyleSheet.create({
+        maincontainer: {
+          flex: 1,
+          backgroundColor: '#10002B',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      });
+      
+      export default ProfilePage;
--- router/AuthStack.tsx
+++ router/AuthStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginPage from "../pages/Login";
+import LoginPage from "../pages/LoginPage";
 
 const Stack = createNativeStackNavigator();
 
